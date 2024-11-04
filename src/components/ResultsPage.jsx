f798d70cd217904aa40dd8cb413f154cdff2a25d
hnihat01 <medzhidh@abv.bg>
2024-11-04 17:26:34 +0200

    Updated

--- src/components/ResultsPage.jsx
+++ src/components/ResultsPage.jsx
@@ -17,10 +17,10 @@ const ResultsPage = () => {
             <div className="text-container">
                 <p className="first-text">Build your everyday self<br /> care routine.</p>
                 <p className="second-text">
-                    Perfect for if you're looking for soft, nourished skin, our moisturizing body<br />
-                    washes are made with skin-natural nutrients that work with your skin to <br />
-                    replenish moisture. With a light formula, the bubbly lather leaves your skin<br />
-                    feeling cleansed and cared for. And by choosing relaxing fragrances you can <br />
+                    Perfect for if you're looking for soft, nourished skin, our moisturizing body
+                    washes are made with skin-natural nutrients that work with your skin to 
+                    replenish moisture. With a light formula, the bubbly lather leaves your skin
+                    feeling cleansed and cared for. And by choosing relaxing fragrances you can 
                     add a moment of calm to the end of your day.
                 </p>
                 <button className="button-style" onClick={handleRetakeQuiz}>Retake the quiz</button>
